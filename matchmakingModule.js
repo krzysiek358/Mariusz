a7c6a3322691168ea9b310a3972fe153e25cc157
krzysiek <krzysztof.stokop@onet.pl>
2018-03-23 13:19:20 +0100

    test

--- matchmakingModule.js
+++ matchmakingModule.js
@@ -10,16 +10,16 @@ function matchmaking(player, games)				//przydzielanie graczy
 {
 	for (var i = 0; i < games.length; i++) 
 	{
-		if(games[i].Socket1.client = null)
+		if(games[i].Socket1.client == null)
 			games[i].Socket1.client = player;
 
-		else if(games[i].Socket2.client = null)
+		else if(games[i].Socket2.client == null)
 			games[i].Socket2.client = player;
 
-		else if(games[i].Socket3.client = null)
+		else if(games[i].Socket3.client == null)
 			games[i].Socket3.client = player;
 
-		else if(games[i].Socket4.client = null)
+		else if(games[i].Socket4.client == null)
 			games[i].Socket4.client = player;
 	}
 }

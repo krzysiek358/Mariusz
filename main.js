b239d55487b077e0f4171e0f14fc7959a9812d56
krzysiek <krzysztof.stokop@onet.pl>
2018-03-23 12:16:34 +0100

    test

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ server.on("connection", function(socket)
 		var code = data.toString('utf8', 0, 2);
 		var value;
 		console.log(data.toString('utf8'));
+		console.log(code);
 
 		switch(code)
 		{

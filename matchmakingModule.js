debafb372025f21482885f79a5e8bf1a08f4c2a2
krzysiek <krzysztof.stokop@onet.pl>
2018-05-09 10:15:03 +0200

    test

--- matchmakingModule.js
+++ matchmakingModule.js
@@ -34,7 +34,7 @@ function matchmaking(player, games)				//przydzielanie graczy
 			break;
 		}
 
-		terminal.cli( 100, games[i].Socket1.client, games[i].Socket2.client)
+		//terminal.cli( 100, games[i].Socket1.client, games[i].Socket2.client)
 	}
 }
 
